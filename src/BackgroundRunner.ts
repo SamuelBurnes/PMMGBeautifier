08509ce5fdb031debc5e75341f5682b46e8fa66d
Omegano <Omegano@users.noreply.github.com>
2023-12-01 20:11:39 -0500

    Update BackgroundRunner.ts

--- src/BackgroundRunner.ts
+++ src/BackgroundRunner.ts
@@ -1,60 +1,77 @@
-import {setSettings} from "./util";
+import { setSettings }
+from "./util";
 
 // Get corp prices asynchronously
 // "prices" will contain the prices after the end of the web request
 export function getPrices(webData, sheetURL, sheetName)
 {
 	// Return if the web app ID is missing
-	if(!sheetURL || !sheetName){return;}
-	
+	if (!sheetURL || !sheetName)
+	{
+		return;
+	}
+
 	// Create a new XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: Custom Prices Timeout");
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				webData["custom_prices"] = {};
-				// Try to parse data received into prices
+
 				console.log("PMMG: Retreived Custom Prices");
+
+				// Try to parse data received into prices
 				var priceData = JSON.parse(xhr.responseText);
-				if(priceData.error)
+				if (priceData.error)
 				{
 					console.log("PMMG: Custom Prices " + priceData.error);
 					return;
 				}
-				else if(!priceData.prices)
+				else if (!priceData.prices)
 				{
 					console.log("PMMG: No Data from Custom Prices");
 				}
-				priceData.prices.forEach(price => {	// Copy data into prices array, preserving the original object
+
+				// Copy data into prices array, preserving the original object
+				priceData.prices.forEach(
+					price =>
+					{
 					webData["custom_prices"][price[0]] = price[1];
-				});
+					}
+				);
 				console.log(webData["custom_prices"]);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from Custom Prices");
 			}
 		}
+
 		return;
-    };
-    // Parse the URL
+	};
+
+	// Parse the URL
 	const sheetID = sheetURL.match(/\/d\/([^\/]+)/);
-	if(!sheetID || !sheetID[1])
+	if (!sheetID || !sheetID[1])
 	{
 		return;
 	}
-	
+
 	// Make the request
+	var address = "https://script.google.com/macros/s/AKfycbwdxGx-OBVslFeXSSv-F_d55X_BTPs20vTMNiT8D9eIAkbcckXh9XAkX9fdBMIv1XrY/exec?id=" + sheetID[1] + "&sheet=" + sheetName;
 	xhr.timeout = 10000;
-	xhr.open("GET", "https://script.google.com/macros/s/AKfycbwdxGx-OBVslFeXSSv-F_d55X_BTPs20vTMNiT8D9eIAkbcckXh9XAkX9fdBMIv1XrY/exec?id=" + sheetID[1] + "&sheet=" + sheetName, true);
+
+	xhr.open("GET", address, true);
 	xhr.send(null);
+
 	return;
 }
 
@@ -64,49 +81,58 @@ export function getCXPrices(userInfo)
 {
 	// Create a new XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: CX Price Timeout");
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				// Parse the results
 				console.log("PMMG: Retreived CX Prices");
 				var priceData = JSON.parse(xhr.responseText);
-				const wantedResults = ["AskPrice", "BidPrice", "Average", "AskAvail", "BidAvail"];	// The data points for each CX to pull
-				const CXs = ["AI1", "CI1", "CI2", "IC1", "NC1", "NC2"];	// All the CXs
-				
+
+				// The data points for each CX to pull
+				const wantedResults = ["AskPrice", "BidPrice", "Average", "AskAvail", "BidAvail"];
+				const CXs = ["AI1", "CI1", "CI2", "IC1", "NC1", "NC2"]; // All the CXs
+
 				userInfo["PMMG-User-Info"]["cx_prices"] = {};
-				
-				CXs.forEach(CX => {
+
+				CXs.forEach(CX =>
+				{
 					userInfo["PMMG-User-Info"]["cx_prices"][CX] = {};
-					wantedResults.forEach(wanted => {
+					wantedResults.forEach(wanted =>
+					{
 						userInfo["PMMG-User-Info"]["cx_prices"][CX][wanted] = {};
-						priceData.forEach(mat => {
+						priceData.forEach(mat =>
+						{
 							userInfo["PMMG-User-Info"]["cx_prices"][CX][wanted][mat["Ticker"]] = mat[CX + "-" + wanted];
 						});
 					});
 				});
-				
-				userInfo["PMMG-User-Info"]["cx_prices"]["Age"] = Date.now();	// Date the data
+
+				// Date the data
+				userInfo["PMMG-User-Info"]["cx_prices"]["Age"] = Date.now();
 				setSettings(userInfo);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from Rain Prices");
 			}
 		}
+
 		return;
-    };
-    
+	};
+
 	// Send the request
 	xhr.timeout = 10000;
 	xhr.open("GET", "https://rest.fnar.net" + "/rain/prices", true);
 	xhr.send(null);
+
 	return;
 }
 
@@ -114,45 +140,64 @@ export function getCXPrices(userInfo)
 // "burn" will contain the burn at the end of the web request
 export function getBurn(webData, username, apikey)
 {
-	if(!webData["burn"]){webData["burn"] = {};}
-	if(!apikey || !username){return;}	// If API key or username is missing, abort
+	if (!webData["burn"])
+	{
+		webData["burn"] = {};
+	}
+
+	// If API key or username is missing, abort
+	if (!apikey || !username)
+	{
+		return;
+	}
 	webData["burn"][username] = [];
-	
+
 	// Create an XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {	// On timeout, try again
+
+	// On timeout, try again
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: FIO Burn Timeout");
 		webData["burn"][username] = undefined;
 		//getBurn(burn, username, apikey);
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				// Parse the results
 				console.log("PMMG: Retreived Burn from FIO");
 				var burnData = JSON.parse(xhr.responseText);
-				burnData.forEach(data => {	// Copy the data into the burn variable
-					webData["burn"][username].push(data);
-				});
+
+				// Copy the data into the burn variable
+				burnData.forEach(
+					data =>
+					{
+						webData["burn"][username].push(data);
+					}
+				);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from FIO");
 				webData["burn"][username] = undefined;
 			}
 		}
+
 		return;
-    };
-    
+	};
+
 	// Send the request
 	xhr.timeout = 20000;
-	xhr.open("GET", "https://rest.fnar.net" + "/fioweb/burn/user/" + username, true);
-    xhr.setRequestHeader("Authorization", apikey);
-    xhr.send(null);
+	var address = "https://rest.fnar.net" + "/fioweb/burn/user/" + username;
+	xhr.open("GET", address, true);
+	xhr.setRequestHeader("Authorization", apikey);
+	xhr.send(null);
+
 	return;
 }
 
@@ -160,45 +205,60 @@ export function getBurn(webData, username, apikey)
 // "burn" will contain the burn at the end of the web request
 export function getGroupBurn(webData, groupid, apikey)
 {
-	if(!webData["burn"]){webData["burn"] = {};}
-	if(!apikey || !groupid){return;}
-	
+	if (!webData["burn"])
+	{
+		webData["burn"] = {};
+	}
+	if (!apikey || !groupid)
+	{
+		return;
+	}
+
 	// Create an XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: FIO Burn Timeout");
 		webData["burn"][groupid] = undefined;
 		//getGroupBurn(burn, groupid, apikey);
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				// Parse the results
 				console.log("PMMG: Retreived Group Burn from FIO");
 				var burnData = JSON.parse(xhr.responseText);
 				webData["burn"][groupid] = [];
-				burnData.forEach(data => { // Copy the results to the burn array
+
+				// Copy the results to the burn array
+				burnData.forEach(
+					data =>
+					{
 					webData["burn"][groupid].push(data);
-				});
+					}
+				);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from FIO");
 				webData["burn"][groupid] = undefined;
 			}
 		}
+
 		return;
-    };
-    
+	};
+
 	// Send the request
 	xhr.timeout = 20000;
-	xhr.open("GET", "https://rest.fnar.net" + "/fioweb/burn/group/" + groupid, true);
-    xhr.setRequestHeader("Authorization", apikey);
-    xhr.send(null);
+	var address = "https://rest.fnar.net" + "/fioweb/burn/group/" + groupid;
+	xhr.open("GET", address, true);
+	xhr.setRequestHeader("Authorization", apikey);
+	xhr.send(null);
+
 	return;
 }
 
@@ -207,43 +267,62 @@ export function getGroupBurn(webData, groupid, apikey)
 export function getBurnSettings(webData, username, apikey)
 {
 	webData["burn_settings"] = [];
-	webData["burn_settings"].push("loading");	// Push a temporary value to the array to allow for easier parsing
-	if(!apikey || !username){return;}
-	
+
+	// Push a temporary value to the array to allow for easier parsing
+	webData["burn_settings"].push("loading");
+
+	if (!apikey || !username)
+	{
+		return;
+	}
+
 	// Create an XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: FIO Burn Settings Timeout");
 		//getBurnSettings(burnSettings, username, apikey);
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				// Parse the results
 				console.log("PMMG: Retreived Burn Settings from FIO");
 				webData["burn_settings"][0] = "loaded";
+
 				var burnData = JSON.parse(xhr.responseText);
-				burnData.forEach(data => {	// Copy the settings data
-					webData["burn_settings"].push(data);
-				});
+
+				// Copy the settings data
+				burnData.forEach(
+					data =>
+					{
+						webData["burn_settings"].push(data);
+					}
+				);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from FIO");
 			}
 		}
+
 		return;
-    };
-    
+	};
+
 	// Send the request
 	xhr.timeout = 10000;
-	xhr.open("GET", "https://rest.fnar.net" + "/usersettings/burnrate/" + username, true);
-    xhr.setRequestHeader("Authorization", apikey);
-    xhr.send(null);
+
+	var address = "https://rest.fnar.net" + "/usersettings/burnrate/" + username;
+
+	xhr.open("GET", address, true);
+	xhr.setRequestHeader("Authorization", apikey);
+	xhr.send(null);
+
 	return;
 }
 
@@ -251,43 +330,57 @@ export function getBurnSettings(webData, username, apikey)
 // "cxos" will contain the settings at the end of the web request
 export function getCXOS(webData, username, apikey)
 {
-	if(!apikey || !username){return;}
-	
+	if (!apikey || !username)
+	{
+		return;
+	}
+
 	// Create an XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: FIO CXOS Timeout");
 		//getBurnSettings(burnSettings, username, apikey);
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
-				
 				// Parse the results
 				console.log("PMMG: Retreived CXOS from FIO");
 				var cxosData = JSON.parse(xhr.responseText);
 				webData["cxos"] = [];
-				cxosData.forEach(data => {	// Copy the settings data
-					webData["cxos"].push(data);
-				});
+
+				// Copy the settings data
+				cxosData.forEach(
+					data =>
+					{
+						webData["cxos"].push(data);
+					}
+				);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from FIO");
 			}
 		}
+
 		return;
-    };
-    
+	};
+
 	// Send the request
 	xhr.timeout = 10000;
-	xhr.open("GET", "https://rest.fnar.net" + "/cxos/" + username, true);
-    xhr.setRequestHeader("Authorization", apikey);
-    xhr.send(null);
+
+	var address = "https://rest.fnar.net" + "/cxos/" + username;
+
+	xhr.open("GET", address, true);
+	xhr.setRequestHeader("Authorization", apikey);
+	xhr.send(null);
+
 	return;
 }
 
@@ -295,43 +388,65 @@ export function getCXOS(webData, username, apikey)
 // "burnSettings" will contain the settings at the end of the web request
 export function getContracts(webData, username, apikey)
 {
-	if(!webData["contracts"]){webData["contracts"] = {};}
-	if(!apikey || !username){return;}	// If API key or username is missing, abort
+	if (!webData["contracts"])
+	{
+		webData["contracts"] = {};
+	}
+
+	if (!apikey || !username) // If API key or username is missing, abort
+	{
+		return;
+	}
+
 	webData["contracts"][username] = [];
-	
+
 	// Create an XML Http Request
 	var xhr = new XMLHttpRequest();
-	xhr.ontimeout = function () {	// On timeout, try again
+
+	// On timeout, try again
+	xhr.ontimeout = function ()
+	{
 		console.log("PMMG: FIO Contract Timeout");
 		webData["contracts"][username] = undefined;
 		//getContracts(contracts, username, apikey);
 	};
-	
-	xhr.onreadystatechange = function()
-    {
-	    if(xhr.readyState == XMLHttpRequest.DONE)
-	    {
+
+	xhr.onreadystatechange = function ()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
 			try
 			{
 				// Parse the results
 				console.log("PMMG: Retreived Contracts from FIO");
 				var burnData = JSON.parse(xhr.responseText);
-				burnData.forEach(data => {	// Copy the data into the contract variable
-					webData["contracts"][username].push(data);
-				});
+
+				// Copy the data into the contract variable
+				burnData.forEach(
+					data =>
+					{
+						webData["contracts"][username].push(data);
+					}
+				);
 			}
-			catch(SyntaxError)
+			catch (SyntaxError)
 			{
 				console.log("PMMG: Bad Data from FIO");
 				webData["contracts"][username] = undefined;
 			}
 		}
+
 		return;
-    };
+	};
+
 	// Send the request
 	xhr.timeout = 20000;
-	xhr.open("GET", "https://rest.fnar.net" + "/contract/allcontracts");
-    xhr.setRequestHeader("Authorization", apikey);
-    xhr.send(null);
+
+	var address = "https://rest.fnar.net" + "/contract/allcontracts";
+
+	xhr.open("GET", address);
+	xhr.setRequestHeader("Authorization", apikey);
+	xhr.send(null);
+
 	return;
 }
